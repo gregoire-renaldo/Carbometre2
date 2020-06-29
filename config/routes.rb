e443b2aa8cec64163d9afc58c85a6298a92100c2
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-06-29 10:34:04 +0200

    seed ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/show'
+  get 'items/new'
+  get 'items/create'
+  get 'items/edit'
+  get 'items/update'
+  get 'items/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,59 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Cleaning database...'
+
+Item.destroy_all
+List.destroy_all
+Footprint.destroy_all
+
+
+puts 'Creating 20 fake users...'
+list = (0..21).to_a
+list.each do |num|
+    user = User.new(
+      email:"#{num}@gmail.com",
+      password: 'azerty',
+    )
+    user.save!
+end
+puts 'Finished!'
+
+puts 'Creating 20 fake footprints...'
+40.times do
+  footprint = Footprint.new(
+    user_id: rand(1..20)
+  )
+  footprint.save!
+end
+puts 'Finished!'
+
+puts 'creating 20 items'
+20.times do
+  item = Item.new(
+    category: Faker::Subscription.plan,
+    charge: rand(0.10..10.32)
+  )
+  item.save
+end
+puts 'finished'
+
+
+
+puts 'Creating 30 fake lists (bilans carbone)...'
+20.times do
+  bilan = List.new(
+    footprint_id: rand(0..20),
+    item_id: rand(1..20),
+    value: rand(1..50),
+    value2: 0
+  )
+  bilan.save!
+end
+puts 'Finished!'
+
+
+
+
