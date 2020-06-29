ce51ea6b0d87bfd674a90eb32627135cf8af51a1
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-06-29 12:10:02 +0200

    merge

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,54 @@
+class ListsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
+
+
+# class DosesController < ApplicationController
+#   # def new
+#   #   @cocktail = Cocktail.find(params[:cocktail_id])
+#   #   @dose = Dose.new
+#   # end
+
+#   def create
+#     @cocktail = Cocktail.find(params[:cocktail_id])
+#     @dose = Dose.new(dose_params)
+#     @dose.cocktail = @cocktail
+#     if @dose.save
+#       redirect_to cocktail_path(@cocktail)
+#     else
+#       @review = Review.new
+#       render "cocktails/show"
+#     end
+#   end
+
+#   def destroy
+#     @dose = Dose.find(params[:id])
+#     @dose.destroy
+#     redirect_to cocktail_path(@dose.cocktail)
+#   end
+
+#   private
+
+#   def dose_params
+#     params.require(:dose).permit(:description, :ingredient_id)
+#   end
+# end
